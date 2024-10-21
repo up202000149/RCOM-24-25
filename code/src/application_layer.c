5b538c857a9e1e9c713996964234a8a4a30e191e
up202000149 <114242390+up202000149@users.noreply.github.com>
2024-10-21 16:09:43 +0100

    Update application_layer.c

--- code/src/application_layer.c
+++ code/src/application_layer.c
@@ -1,10 +1,25 @@
 // Application layer protocol implementation
 
 #include "application_layer.h"
+#include "link_layer.h"
+#include <string.h>
 
 void applicationLayer(const char *serialPort, const char *role, int baudRate,
                       int nTries, int timeout, const char *filename)
 {
+    LinkLayer parameters;
+    strcpy(parameters.serialPort, serialPort);
+    if(!strcmp(role,"rx")){
+        parameters.role = LlRx;
+    }else if(!strcmp(role,"tx")){
+        parameters.role = LlTx;
+    }
+    parameters.baudRate = baudRate;
+    parameters.nRetransmissions = nTries;
+    parameters.timeout = timeout;
+    
+
+    llopen(parameters);
     /* TODO: 
     open connection 
     if write
