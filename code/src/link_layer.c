46307a5ba5ae747c1d2c42e0080ebee06044bd50
up202000149 <114242390+up202000149@users.noreply.github.com>
2024-10-21 14:58:51 +0100

    Update link_layer.c

--- code/src/link_layer.c
+++ code/src/link_layer.c
@@ -16,7 +16,38 @@ int llopen(LinkLayer connectionParameters)
     {
         return -1;
     }
+    unsigned char buf[BUF_SIZE + 1] = {0};
+    
+    switch (connectionParameters.role)
+    {
+    case LlRx:
+        readByte(buf);
+        printf("var = 0x%02X\n", buf[0]);
+        printf("var = 0x%02X\n", buf[1]);
+        printf("var = 0x%02X\n", buf[2]);
+        printf("var = 0x%02X\n", buf[3]);
+        printf("var = 0x%02X\n", buf[4]);
+        buf[2] = UA;
+        buf[3] = buf[1] ^ buf[2];
 
+        writeBytes(buf, BUF_SIZE);
+        break;
+    case LlTx:
+        buf[0] = FLAG;
+        buf[1] = ADDR_SEN;
+        buf[2] = SET;
+        buf[3] = ADDR_SEN ^ SET;
+        buf[4] = FLAG;
+
+        writeBytes(buf, BUF_SIZE);
+        readByte(buf);
+        printf("var = 0x%02X\n", buf[0]);
+        printf("var = 0x%02X\n", buf[1]);
+        printf("var = 0x%02X\n", buf[2]);
+        printf("var = 0x%02X\n", buf[3]);
+        printf("var = 0x%02X\n", buf[4]);
+        break;
+    }
     /* TODO:
         
     */
