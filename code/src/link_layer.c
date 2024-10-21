1fba4f1240c090292fc3dc23d3d8c53e0dc27d1d
André Teixeira <114063798+up202108882@users.noreply.github.com>
2024-10-21 15:42:07 +0100

    Update link_layer.c

--- code/src/link_layer.c
+++ code/src/link_layer.c
@@ -2,13 +2,87 @@
 
 #include "link_layer.h"
 #include "serial_port.h"
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <termios.h>
+#include <unistd.h>
 
 // MISC
 #define _POSIX_SOURCE 1 // POSIX compliant source
+#define FLAG 0x7E
+#define ANSREC 0x03
+#define ANSSEN 0x01
+#define CTRLSET 0x03
+#define CTRLUA 0x07
+#define RR0 0xAA
+#define RR1 0xAB
+#define REJ0 0x54
+#define REJ1 0x55
+#define DISC 0x0B
+
+#define MAX_ALARM_COUNT 3
+#define FALSE 0
+#define TRUE 1
+#define BUF_SIZE 256
+
+int alarmEnabled = FALSE;
+int alarmCount = 0;
+volatile int STOP = FALSE;
 
 ////////////////////////////////////////////////
 // LLOPEN
 ////////////////////////////////////////////////
+
+enum State { FLAG_REC, A_REC, C_REC, BCC_OK, FLAG_REC_END };
+
+void processState(enum State *curState, unsigned char currByte) {
+    switch (*curState) {
+        case FLAG_REC:
+            if (currByte == FLAG) *curState = A_REC;
+            break;
+        case A_REC:
+            if (currByte == ANSSEN) *curState = C_REC;
+            else *curState = FLAG_REC;
+            break;
+        case C_REC:
+            if (currByte == CTRLSET) *curState = BCC_OK;
+            else *curState = FLAG_REC; 
+            break;
+        case BCC_OK:
+            if (currByte == (ANSSEN ^ CTRLSET)) *curState = FLAG_REC_END;
+            else *curState = FLAG_REC;
+            break;
+        case FLAG_REC_END:
+            if (currByte == FLAG) {
+                printf("Frame received\n");
+            }
+            *curState = FLAG_REC;
+            break;
+    }
+}
+
+void alarmHandler(int signal) {
+    alarmCount++;
+    if (alarmCount < MAX_ALARM_COUNT) {
+        buf[0] = FLAG;
+        buf[1] = ANSSEN;
+        buf[2] = CTRLSET;
+        buf[3] = ANSSEN ^ CTRLSET;
+        buf[4] = FLAG;
+
+        writeBytes(buf, 5);
+
+        alarm(3);       
+    } else {
+        printf("Max retry limit reached. Exiting...\n");
+        exit(1); 
+    }
+}
+
 int llopen(LinkLayer connectionParameters)
 {
     if (openSerialPort(connectionParameters.serialPort,
@@ -16,41 +90,61 @@ int llopen(LinkLayer connectionParameters)
     {
         return -1;
     }
-    unsigned char buf[BUF_SIZE + 1] = {0};
-    
-    switch (connectionParameters.role)
-    {
-    case LlRx:
-        readByte(buf);
-        printf("var = 0x%02X\n", buf[0]);
-        printf("var = 0x%02X\n", buf[1]);
-        printf("var = 0x%02X\n", buf[2]);
-        printf("var = 0x%02X\n", buf[3]);
-        printf("var = 0x%02X\n", buf[4]);
-        buf[2] = UA;
-        buf[3] = buf[1] ^ buf[2];
 
-        writeBytes(buf, BUF_SIZE);
-        break;
-    case LlTx:
+    unsigned char buf[BUF_SIZE + 1] = {0}; 
+
+    if(connectionParameters.role==LlTx){
         buf[0] = FLAG;
-        buf[1] = ADDR_SEN;
-        buf[2] = SET;
-        buf[3] = ADDR_SEN ^ SET;
+        buf[1] = ANSSEN;
+        buf[2] = CTRLSET;
+        buf[3] = ANSSEN ^ CTRLSET;
         buf[4] = FLAG;
 
-        writeBytes(buf, BUF_SIZE);
-        readByte(buf);
-        printf("var = 0x%02X\n", buf[0]);
-        printf("var = 0x%02X\n", buf[1]);
-        printf("var = 0x%02X\n", buf[2]);
-        printf("var = 0x%02X\n", buf[3]);
-        printf("var = 0x%02X\n", buf[4]);
-        break;
+        writeBytes(buf, 5);
+
+        alarm(3);
+
+        int bytes = 0;
+
+        for (int i = 0; i < 5; i++) {
+            bytes += readByte(&buf[i]);
+            printf("%d bytes read\n", bytes);
+        }
+
+        if (bytes = 5 && buf[0] = FLAG && buf[1] = ANSREC && buf[2] = CTRLUA && buf[3] = ANSREC ^ CTRLUA && buf[4] = FLAG) {
+            printf("Success\n");
+            alarm(0);
+            break;
+        }
+        else {
+            printf("Error\n");
+        }
     }
-    /* TODO:
+
+    else {
+        enum State curState = FLAG_REC;
+
+        while (!STOP) {
+            int bytes = readByte(&buf[0]); 
+
+            if (bytes > 0) {
+                processState(&curState, buf[0]);
+
+                if (curState == FLAG_REC_END) {
+                    printf("sending UA\n");
+                    unsigned char response[5] = {FLAG, ANSREC, CTRLUA, ANSREC ^ CTRLUA, FLAG};
+                    writeBytes(response, 5);
+                    printf("UA frame sent\n");
+                    STOP = TRUE;
+                }
+            }
+        }
+    }
+
+    return -1;
+
         
-    */
+}
 
     return 1;
 }
@@ -60,13 +154,7 @@ int llopen(LinkLayer connectionParameters)
 ////////////////////////////////////////////////
 int llwrite(const unsigned char *buf, int bufSize)
 {
-    /* TODO:
-    generate bcc2
-    implement n+1 stuffing
-    create i frame
-    send frame
-    enter read cycle
-    */
+    // TODO
 
     return 0;
 }
@@ -76,9 +164,7 @@ int llwrite(const unsigned char *buf, int bufSize)
 ////////////////////////////////////////////////
 int llread(unsigned char *packet)
 {
-    /* TODO:
-    
-    */
+    // TODO
 
     return 0;
 }
@@ -88,10 +174,8 @@ int llread(unsigned char *packet)
 ////////////////////////////////////////////////
 int llclose(int showStatistics)
 {
-    /* TODO:
-    */
+    // TODO
 
     int clstat = closeSerialPort();
     return clstat;
 }
-
